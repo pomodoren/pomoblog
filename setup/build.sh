09dd039d2a9121e2c96ec99b11597a803dbaed5d
DorenCalliku <dcalliku@gmail.com>
2023-11-05 16:38:42 +0100

    Adding presentation on OSINT for journalists

--- setup/build.sh
+++ setup/build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 jupyter nbconvert --to html index.ipynb
-jupyter nbconvert --to slides "2023-11-04-cryptoparty-OSINT for Journalism.ipynb" --reveal-prefix=reveal.js
+jupyter nbconvert --to slides "X.ipynb" --reveal-prefix=reveal.js
